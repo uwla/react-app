f60341753c2d1e4a3c6362221134acb45e31bf87
uwla <uwlalinux@gmail.com>
2024-02-09 15:34:54 -0300

    feat: add dismiss button to alert box

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,21 @@ function App() {
     const items = ["New York", "San Diego", "Chicago", "Boston", "Miami"];
 
     let [selected, setSelected] = useState("");
+    let [showInfo, setInfoVisibility] = useState(false);
 
     const handleSelected = (item: string) => {
         setSelected(item);
+        setInfoVisibility(true);
     };
 
     return (
         <>
-            <If condition={selected != ''}>
-                <Alert variant="info">
+            <If condition={showInfo}>
+                <Alert
+                    variant="info"
+                    dismissible={true}
+                    onDismiss={() => setInfoVisibility(false)}
+                >
                     You selected <b>{selected}</b>
                 </Alert>
             </If>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,16 +1,44 @@
 import { ReactNode } from "react";
+import If from "./If";
 
 interface Props {
     children: ReactNode;
-    variant?:  "danger" | "dark" | "info" | "light" | "success" | "secondary" | "primary" | "warning";
+    variant?:
+        | "danger"
+        | "dark"
+        | "info"
+        | "light"
+        | "primary"
+        | "secondary"
+        | "success"
+        | "warning";
+    dismissible?: boolean;
+    onDismiss?: () => void;
 }
 
 function Alert(props: Props) {
-    const { children, variant = "primary" } = props;
+    const {
+        children,
+        variant = "primary",
+        dismissible = false,
+        onDismiss,
+    } = props;
+
+    let className = `alert alert-${variant}`;
+    if (dismissible) className += " alert-dismissible fade show";
 
     return (
-        <div className={`alert alert-${variant}`} role="alert">
+        <div className={className} role="alert">
             {children}
+            <If condition={dismissible}>
+                <button
+                    type="button"
+                    className="btn-close"
+                    aria-label="close"
+                    data-bs-dismiss="alert"
+                    onClick={onDismiss}
+                ></button>
+            </If>
         </div>
     );
 }
