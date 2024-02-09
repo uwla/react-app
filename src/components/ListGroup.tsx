dea116bfea2fbcd58c674d0a0598af0c3d94ddfe
uwla <uwlalinux@gmail.com>
2024-02-09 10:08:50 -0300

    feat: highlight selected item on click

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
     const items = ["Mary", "John", "Gertrude", "Jeremy", "Grady", "Thomas"];
+
+    let [selected, setIndex] = useState(-1);
+
+    let msg = <p>You selected {items[selected]}</p>;
 
     return (
         <>
             <h1>List Group</h1>
+            <br />
+
+            {selected != -1 && msg}
+
             <ul className="list-group">
                 {items.map((item, index) => (
-                    <li key={index} className="list-group-item">
+                    <li
+                        key={index}
+                        className={
+                            index != selected
+                                ? "list-group-item"
+                                : "list-group-item active"
+                        }
+                        onClick={() => setIndex(index)}
+                    >
                         {item}
                     </li>
                 ))}
